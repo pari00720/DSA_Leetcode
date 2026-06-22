b244d12902c835b11f51d72fee18bed192f72027
Pari Mishra <pariimishra07@gmail.com>
2026-06-22 14:19:35 +0530

    Time: 7 ms (41.26%), Space: 20.5 MB (61.22%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,7 @@
+class Solution:
+    def moveZeroes(self, nums: list[int]) -> None:
+        last_non_zero = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[last_non_zero], nums[i] = nums[i], nums[last_non_zero]
+                last_non_zero += 1
